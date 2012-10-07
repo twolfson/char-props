785c6209feddd031295064e40d1ff7f11ca3491e
Todd Wolfson <todd@twolfson.com>
2012-10-06 20:28:01 -0700

    Working through proper cases for tests

--- test/charProps_test.js
+++ test/charProps_test.js
@@ -1,12 +1,48 @@
 var vows = require('vows'),
+    assert = require('assert'),
     charProps = require('../lib/charProps.js'),
     suite = vows.describe('charProps');
+
+var file = [
+  // line, col, charCount (including line breaks)
+  'line 1', // 0, 5, 5
+  'another second line', // 1, 19, 25 (5 + 1 + 19)
+  '3rd line!', // 2, 9, 35 (25 + 1 + 9)
+  'line fourrrrrrrrr' // 3, 17, 53 (35 + 1 + 17)
+].join('\n');
 
 // Basic tests
 suite.addBatch({
   'A new Indexer': {
-    'can find the line of a character at a given index': '',
-    'can find the column of a character at a given index': ''
+    topic: function () {
+      return charProps(file);
+    },
+    'can find the line of a character at a given index': function (indexer) {
+      var index = 20, // d in 'second'
+          char = file.charAt(index);
+
+      // Sanity check
+      assert.strictEqual(char, 'd', 'The character we are the line of finding is "d"');
+
+      // Grab the line number of char
+      var line = indexer.lineAt(index);
+
+      // Assert the 'second' is on the second line
+      assert.strictEqual(line, 1, 'The character at index 20 is on the second line');
+    },
+    'can find the column of a character at a given index': function (indexer) {
+      var index = 35, // ! in 'line!'
+          char = file.charAt(index);
+
+      // Sanity check
+      assert.strictEqual(char, '!', 'The character we are the column of finding is "!"');
+
+      // Grab the column number of char
+      var col = indexer.columnAt(index);
+
+      // Assert it is in the eigth column
+      assert.strictEqual(col, 8, 'The character at index 35 is in the ninth column');
+    }
   }
 });
 
@@ -14,6 +50,34 @@ suite.addBatch({
 suite.addBatch({
   'A new Indexer': {
     'can find the line of a character at a given index using a minimum line': ''
+  }
+});
+
+// Edge cases
+suite.addBatch({
+  'A new Indexer': {
+    topic: function () {
+      return charProps(file);
+    },
+    'considers a line feed part of the past line': function (indexer) {
+      var index = 26, // \n of line 3
+          char = file.charAt(index);
+
+      // Sanity check
+      assert.strictEqual(char, '\n', 'The character we are the column of finding is a line feed');
+
+      // Grab the line number of char
+      var line = indexer.lineAt(index);
+
+      // Assert it is on the second line
+      assert.strictEqual(line, 1, 'The character us on the second line');
+
+      // Grab the column number of char
+      var col = indexer.columnAt(index);
+
+      // Assert it is in the nineteenth column
+      assert.strictEqual(col, 19, 'The character at index 26 is in the zero-th column');
+    }
   }
 });
 
