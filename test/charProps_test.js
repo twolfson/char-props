4e178d2d91006661644e34f5cc23aa9c69ba61af
Todd Wolfson <todd@twolfson.com>
2012-10-09 00:21:17 -0700

    Caught failing edge case

--- test/charProps_test.js
+++ test/charProps_test.js
@@ -107,13 +107,51 @@ suite.addBatch({
       var line = indexer.lineAt(index);
 
       // Assert it is on the second line
-      assert.strictEqual(line, 1, 'The character us on the second line');
+      assert.strictEqual(line, 1, 'The character at index 26 on the second line');
 
       // Grab the column number of char
       var col = indexer.columnAt(index);
 
       // Assert it is in the nineteenth column
-      assert.strictEqual(col, 19, 'The character at index 26 is in the zero-th column');
+      assert.strictEqual(col, 19, 'The character at index 26 is in the nineteenth column');
+    },
+    'considers the first character of a line to be on that line': function (indexer) {
+      var index = 27, // 3 of line 3
+          char = file.charAt(index);
+
+      // Sanity check
+      assert.strictEqual(char, '3', 'The character we are the positions of finding is 3');
+
+      // Grab the line number of char
+      var line = indexer.lineAt(index);
+
+      // Assert it is on the third line
+      assert.strictEqual(line, 2, 'The character at index 27 on the third line');
+
+      // Grab the column number of char
+      var col = indexer.columnAt(index);
+
+      // Assert it is in the zeroth column
+      assert.strictEqual(col, 0, 'The character at index 27 is in the zero-th column');
+    },
+    'considers the last character of a line to be on that line': function (indexer) {
+      var index = 5, // 1 of line 1
+          char = file.charAt(index);
+
+      // Sanity check
+      assert.strictEqual(char, '1', 'The character we are the positions of finding is 1');
+
+      // Grab the line number of char
+      var line = indexer.lineAt(index);
+
+      // Assert it is on the first line
+      assert.strictEqual(line, 0, 'The character at index 6 on the first line');
+
+      // Grab the column number of char
+      var col = indexer.columnAt(index);
+
+      // Assert it is in the fifth column
+      assert.strictEqual(col, 5, 'The character at index 6 is in the fifth column');
     }
   }
 });
